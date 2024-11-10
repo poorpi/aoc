d4ceaaf80690a3311688070002f00b14a20a12d0
Maciej Zarembski <mackozb@gmail.com>
2024-11-10 23:20:08 +0100

    2024 base

---
+++ 2024/6/code.py
@@ -0,0 +1,22 @@
+# https://adventofcode.com/2024/day/6
+import sys
+import os
+
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+if root_dir not in sys.path:
+    sys.path.insert(0, root_dir)
+
+import aoc_utils as aoc
+
+
+def part1(data):
+    return None
+
+
+def part2(data):
+    return None
+
+    
+data = aoc.get_data(6, 2024)
+print('Part 1:', part1(data))
+print('Part 2:', part2(data))
