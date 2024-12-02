d9f8e40ab7ecb25dffe1bc1b5e7f2f44c24b4516
Maciej Zarembski <mackozb@gmail.com>
2024-12-02 14:15:24 +0100

    day 2

--- 2024/2/code.py
+++ 2024/2/code.py
@@ -8,18 +8,78 @@ if root_dir not in sys.path:
 
 import aoc_utils as aoc
 
-
 def part1(data):
-    return None
+    safe = 0
+    for line in data.splitlines():
+        levels = [int(x) for x in line.split()]
+        order = None
+        is_safe = True
+        for i in range(len(levels) - 1):
+            if order is None:
+                if levels[i] > levels[i + 1]:
+                    order = 'inc'
+                elif levels[i] < levels[i + 1]:
+                    order = 'dec'
+            else:
+                if order == 'inc':
+                    if levels[i] < levels[i + 1]:
+                        is_safe = False
+                        break
+                elif order == 'dec':
+                    if levels[i] > levels[i + 1]:
+                        is_safe = False
+                        break
+            diff = abs(levels[i] - levels[i + 1])
+            if diff < 1 or diff > 3:
+                is_safe = False
+                break
 
+        if is_safe:
+            safe += 1
+
+    return safe
 
 def part2(data):
-    return None
 
+    def solve(levels):
+        order = None
+        for i in range(len(levels) - 1):
+            if order is None:
+                if levels[i] > levels[i + 1]:
+                    order = 'inc'
+                elif levels[i] < levels[i + 1]:
+                    order = 'dec'
+            else:
+                if order == 'inc':
+                    if levels[i] < levels[i + 1]:
+                        return False
+                elif order == 'dec':
+                    if levels[i] > levels[i + 1]:
+                        return False
+            diff = abs(levels[i] - levels[i + 1])
+            if diff < 1 or diff > 3:
+                return False
+        return True
 
-run_sample = True
-sample = '''
-'''
+    safe = 0
+    for line in data.splitlines():
+        levels = [int(x) for x in line.split()]
+        for i in range(len(levels)):
+            d = levels[:]
+            d.pop(i)
+            res = solve(d)
+            if res:
+                safe += 1
+                break
+    return safe
+
+run_sample = False
+sample = '''7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9'''
 data = aoc.get_data(2, 2024)
 if run_sample:
     print('Part 1:', part1(sample))
