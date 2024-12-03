0d840b9b544f3da8b09f5f02e5f1137e5fd00d1e
Maciej Zarembski <mackozb@gmail.com>
2024-12-03 08:13:27 +0100

    day 3

--- 2024/3/code.py
+++ 2024/3/code.py
@@ -10,16 +10,40 @@ import aoc_utils as aoc
 
 
 def part1(data):
-    return None
+    import re
+
+    sum = 0
+    matches = re.findall('mul\(\d{1,3},\d{1,3}\)', data)
+    for match in matches:
+        match = match.replace('mul(', '').replace(')', '')
+        a, b = map(int, match.split(','))
+        sum += a * b
+    return sum
 
 
 def part2(data):
-    return None
+    import re
+
+    sum = 0
+    matches = re.findall('(mul\(\d{1,3},\d{1,3}\))|(don\'t)|(do)', data)
+    disabled = False
+    for match in matches:
+        if match[1] == 'don\'t':
+            disabled = True
+            continue
+        if match[2] == 'do':
+            disabled = False
+            continue
+        if disabled:
+            continue
+        match = match[0].replace('mul(', '').replace(')', '')
+        a, b = map(int, match.split(','))
+        sum += a * b
+    return sum
 
 
-run_sample = True
-sample = '''
-'''
+run_sample = False
+sample = '''xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))'''
 data = aoc.get_data(3, 2024)
 if run_sample:
     print('Part 1:', part1(sample))
