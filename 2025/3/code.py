0abd8592489cbe0e13212f4d1c3ebf9fe874877f
Maciej Zarembski <mackozb@gmail.com>
2025-12-03 11:15:57 +0100

    2025 day 3

--- 2025/3/code.py
+++ 2025/3/code.py
@@ -9,16 +9,37 @@ if root_dir not in sys.path:
 import aoc_utils as aoc
 
 
+def solve(data, dim):
+    sum = 0
+    for bank in data.splitlines():
+        on = [0] * dim
+        bank_length = len(bank)
+        for idx, battery in enumerate(bank):
+            joltage = int(battery)
+            for d in range(dim):
+                if joltage > on[d] and bank_length - idx > dim - 1 - d:
+                    on[d] = joltage
+                    for reset_d in range(d + 1, dim):
+                        on[reset_d] = 0
+                    break
+        partial = int(''.join(str(x) for x in on))
+        sum += partial
+    return sum
+
+
 def part1(data):
-    pass
+    return solve(data, 2)
 
 
 def part2(data):
-    pass
+    return solve(data, 12)
 
 
-run_sample = True
-sample = ''''''
+run_sample = False
+sample = '''987654321111111
+811111111111119
+234234234234278
+818181911112111'''
 data = aoc.get_data(3, 2025)
 if run_sample:
     print('Part 1:', part1(sample))
