33b80a675e68d7ce4f7b3430e536224166dc4a16
Maciej Zarembski <mackozb@gmail.com>
2025-12-04 06:15:16 +0100

    2025 day 4

--- 2025/4/code.py
+++ 2025/4/code.py
@@ -9,16 +9,88 @@ if root_dir not in sys.path:
 import aoc_utils as aoc
 
 
+MOORE = [
+    (-1,-1), (0,-1), (1,-1),
+    (-1, 0),         (1, 0),
+    (-1, 1), (0, 1), (1, 1)
+]
+
+
+def neighborhood(grid, x, y, offsets=None, wrap=False):
+    offsets = offsets or MOORE
+    h, w = len(grid), len(grid[0])
+    for dx, dy in offsets:
+        nx, ny = x + dx, y + dy
+        if wrap:
+            nx %= w
+            ny %= h
+        elif not (0 <= nx < w and 0 <= ny < h):
+            continue
+        yield grid[ny][nx]
+
+
 def part1(data):
-    pass
+    grid = []
+    for line in data.splitlines():
+        grid.append(list(line))
+    width = len(grid[0])
+    height = len(grid)
+    
+    sum = 0
+    for y in range(height):
+        for x in range(width):
+            cell = grid[y][x]
+            if cell != '@':
+                continue
+            neighbors = list(neighborhood(grid, x, y))
+            count = 0
+            for n in neighbors:
+                if n == '@':
+                    count += 1
+            if count < 4:
+                sum += 1
+    return sum
 
 
 def part2(data):
-    pass
+    grid = []
+    for line in data.splitlines():
+        grid.append(list(line))
+    width = len(grid[0])
+    height = len(grid)
+    
+    sum = 0
+    part_sum = 1
+    while part_sum > 0:
+        part_sum = 0
+        for y in range(height):
+            for x in range(width):
+                cell = grid[y][x]
+                if cell != '@':
+                    continue
+                neighbors = list(neighborhood(grid, x, y))
+                count = 0
+                for n in neighbors:
+                    if n == '@':
+                        count += 1
+                if count < 4:
+                    grid[y][x] = '.'
+                    part_sum += 1
+        sum += part_sum
+    return sum
 
 
-run_sample = True
-sample = ''''''
+run_sample = False
+sample = '''..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.'''
 data = aoc.get_data(4, 2025)
 if run_sample:
     print('Part 1:', part1(sample))
@@ -26,3 +98,5 @@ if run_sample:
 else:
     print('Part 1:', part1(data))
     print('Part 2:', part2(data))
+
+# LOL, 06:08:30
